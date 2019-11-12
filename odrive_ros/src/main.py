bbbdc7890c958685c42543c46ee35478a73dbd30
CK Lee <nuc@robotics.soc.srcf.net>
2019-11-12 17:48:45 +0000

    Made odrive ros take launch params for serial no

--- odrive_ros/src/main.py
+++ odrive_ros/src/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import odrive
 from fibre import Logger, Event
 import rospy
@@ -9,18 +9,19 @@ logger = Logger(verbose=False)
 
 
 def did_lose_device(device):
-    print("Lost an odrive!")
+    print(rospy.get_name() + ": Lost an odrive!")
 
 
 def did_discover_device(device):
-    print("Found an odrive!")
+    print(rospy.get_name() + ": Found an odrive!")
     device.__channel__._channel_broken.subscribe(lambda: did_lose_device(device))
     # print(repr(obj))
 
 
 rospy.init_node("odrive")
+print("Well I've inited the node, gonna listen now")
 
-serial_no = "336631563536"
+serial_no = rospy.get_param("~serial_no")
 odrive.find_all("usb", serial_no, did_discover_device, shutdown_token, shutdown_token, logger)
 
 r = rospy.Rate(1)
