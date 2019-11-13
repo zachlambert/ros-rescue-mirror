0d552621e3465ce7eb5195392be8ffdf94caa9dc
rcslaney <rcslaney@gmail.com>
2019-11-13 20:36:57 +0000

    Added enums for odrive errors and made node report diagnostics

--- odrive_ros/src/main.py
+++ odrive_ros/src/main.py
@@ -2,29 +2,78 @@
 import odrive
 from fibre import Logger, Event
 import rospy
+import odrive_enums as oenums
+from diagnostic_msgs.msg import DiagnosticStatus, KeyValue
 
 shutdown_token = Event()
 
 logger = Logger(verbose=False)
 
+my_odrive = None
+
+diagnostic_pub = rospy.Publisher('odrivediag', DiagnosticStatus, queue_size=10)
+
+
+def diagnostics():
+    global serial_no
+    message = DiagnosticStatus()
+    message.name = "ODrive report"
+    message.message = "ODrive diagnostics"
+    message.hardware_id = serial_no
+    if my_odrive is None:
+        message.level = 1
+        message.values = [KeyValue("connected", False)]
+    else:
+        message.values = [
+            KeyValue("connected", True),
+            KeyValue("bus-voltage", my_odrive.vbus_voltage),
+            KeyValue("axis0-error", oenums.errors.axis[my_odrive.axis0.error]),
+            KeyValue("axis0-encoder-error", oenums.errors.encoder[my_odrive.axis0.encoder.error]),
+            KeyValue("axis0-motor-error", oenums.errors.motor[my_odrive.axis0.motor.error]),
+            # KeyValue("axis0-controller-error", oenums.errors.controller[my_odrive.axis0.controller.error]),
+            KeyValue("axis0-ctrl-mode", oenums.control_modes[my_odrive.axis0.controller.config.control_mode]),
+            KeyValue("axis0-state", oenums.axis_states[my_odrive.axis0.current_state]),
+            KeyValue("axis1-error", oenums.errors.axis[my_odrive.axis1.error]),
+            KeyValue("axis1-encoder-error", oenums.errors.encoder[my_odrive.axis1.encoder.error]),
+            KeyValue("axis1-motor-error", oenums.errors.motor[my_odrive.axis1.motor.error]),
+            # KeyValue("axis1-controller-error", oenums.errors.controller[my_odrive.axis1.controller.error]),
+            KeyValue("axis1-ctrl-mode", oenums.control_modes[my_odrive.axis1.controller.config.control_mode]),
+            KeyValue("axis1-state", oenums.axis_states[my_odrive.axis1.current_state]),
+        ]
+
+    print(repr(message))
+    diagnostic_pub.publish(message)
+
+
+def clear_errors(axis):
+    axis.error = 0
+    axis.encoder.error = 0
+    axis.motor.error = 0
+    axis.controller.error = 0
+
 
 def did_lose_device(device):
+    global my_odrive
     print(rospy.get_name() + ": Lost an odrive!")
+    my_odrive = None
 
 
 def did_discover_device(device):
+    global my_odrive
     print(rospy.get_name() + ": Found an odrive!")
     device.__channel__._channel_broken.subscribe(lambda: did_lose_device(device))
-    # print(repr(obj))
+    my_odrive = device
 
 
 rospy.init_node("odrive")
 print("Well I've inited the node, gonna listen now")
 
 serial_no = rospy.get_param("~serial_no")
+
 odrive.find_all("usb", serial_no, did_discover_device, shutdown_token, shutdown_token, logger)
 
 r = rospy.Rate(1)
 while not rospy.core.is_shutdown():
-   r.sleep()
+    r.sleep()
+    diagnostics()
 
