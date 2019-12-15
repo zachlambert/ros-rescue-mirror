92528b4f61fcfb4aeeacece43e6356eca06e5ec7
rcslaney <rcslaney@gmail.com>
2019-12-15 14:58:02 +0000

    Continuing arm work

--- controller_mapper/src/main.py
+++ controller_mapper/src/main.py
@@ -15,9 +15,9 @@ def receive_joy_data(message):
     drive_left.publish(-(message.axes[1] * 8192 * 16 + message.axes[0] * 16 * 8192))
     drive_right.publish(message.axes[1] * 8192 * 16 - message.axes[0] * 16 * 8192)
     if message.buttons[4]:
-        arm_values[0] += 1
-    elif message.buttons[1]:
-        arm_values[0] -= 1
+        arm_values[0] += 0.1
+    elif message.buttons[5]:
+        arm_values[0] -= 0.1
     arm_msg.data = arm_values
     arm.publish(arm_msg)
 
@@ -27,11 +27,12 @@ def receive_arm_data(message):
     arm_values = message.data
     print(repr(arm_values))
 
+
 # base button 5 and 4
 rospy.init_node("controller_mapper")
 
 rospy.Subscriber("/gamepad/values", Joy, receive_joy_data)
-rospy.Subscriber("/arm_current_values", Float32MultiArray, receive_arm_data)
+# rospy.Subscriber("/arm_current_values", Float32MultiArray, receive_arm_data)
 
 drive_left = rospy.Publisher('/odrive_drive/axis0/vel_setpoint', Int32, queue_size=10)
 drive_right = rospy.Publisher('/odrive_drive/axis1/vel_setpoint', Int32, queue_size=10)
