9c584e8acb9f4b521ce8d23298fd061495e0c9ff
rcslaney <rcslaney@gmail.com>
2019-12-15 14:42:47 +0000

    Starting to add arm functionality and teensy connections

--- controller_mapper/src/main.py
+++ controller_mapper/src/main.py
@@ -2,20 +2,33 @@
 import rospy
 from sensor_msgs.msg import Joy
 from std_msgs.msg import Int32
+from std_msgs.msg import Float32MultiArray
+
+arm_values = [0, 0, 0]
 
 
-def receive_data(message):
+def receive_joy_data(message):
     global odrive_pub
     print(message.axes[1])
     drive_left.publish(-(message.axes[1] * 8192 * 16 + message.axes[0] * 16 * 8192))
     drive_right.publish(message.axes[1] * 8192 * 16 - message.axes[0] * 16 * 8192)
 
 
+def receive_arm_data(message):
+    global arm_values
+    arm_values = message.data
+    print(repr(arm_values))
+
+
 rospy.init_node("controller_mapper")
 
-rospy.Subscriber("/gamepad/values", Joy, receive_data)
+rospy.Subscriber("/gamepad/values", Joy, receive_joy_data)
+rospy.Subscriber("/arm_current_values", Float32MultiArray, receive_arm_data)
 
 drive_left = rospy.Publisher('/odrive_drive/axis0/vel_setpoint', Int32, queue_size=10)
 drive_right = rospy.Publisher('/odrive_drive/axis1/vel_setpoint', Int32, queue_size=10)
+flipper_left = rospy.Publisher('/odrive_flipper/axis0/vel_setpoint', Int32, queue_size=10)
+flipper_right = rospy.Publisher('/odrive_flipper/axis1/vel_setpoint', Int32, queue_size=10)
+arm = rospy.Publisher('/arm_demand_angles', Float32MultiArray, queue_size=10)
 
 rospy.spin()
