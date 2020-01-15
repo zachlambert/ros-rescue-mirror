c9cff401b3f8800c42c91d126409f585f75f949c
Zach Lambert <zachalambert@gmail.com>
2020-01-15 11:28:42 +0000

    State publisher subscribes to relevant topics.

--- state_publisher/src/state_publisher.py
+++ state_publisher/src/state_publisher.py
@@ -3,33 +3,67 @@
 import rospy
 import tf
 
-from geometry_msgs.msg import TransformStamped
-from std_msgs.msg import Float32
+from geometry_msgs.msg import Vector3
+from std_msgs.msg import Float32, Float32MultiArray
 from sensor_msgs.msg import JointState
 
-import numpy as np
-
-rospy.init_node('robot_tf_broadcaster')
-joint_pub = rospy.Publisher('joint_states', JointState, queue_size=1)
-broadcaster = tf.TransformBroadcaster()
-rate = rospy.Rate(10)
-
-joint_state = JointState()
-
-joint_names = ['base_link_to_flipper_front_right',
-               'base_link_to_arm_lower',
-               'arm_lower_to_arm_upper']
-joint_angles = [0, 0, 0]
+# Callback functions
 
 robot_x = 0
 robot_y = 0
 robot_angle = 0
 
+joints = {
+    'flipper_front_right': 0,
+    'flipper_front_left': 0,
+    'flipper_rear_right': 0,
+    'flipper_rear_left': 0,
+    'arm_pan': 0,
+    'arm_lower_joint': 0,
+    'arm_upper_joint': 0
+}
+
+def _position_callback(data):
+    global robot_x, robot_y, robot_angle
+    robot_x = data.x
+    robot_y = data.y
+    robot_angle = data.angle
+
+def _arm_angles_callback(data):
+    joints['arm_pan'] = data.x
+    joints['arm_lower_joint'] = data.y
+    joints['arm_upper_joint'] = data.z
+
+def _flippers_callback(data):
+    front_angle = data.data[0]
+    rear_angle = data.data[1]
+    joints['flipper_front_right'] = front_angle
+    joints['flipper_front_left'] = front_angle
+    joints['flipper_rear_right'] = rear_angle
+    joints['flipper_rear_left'] = rear_angle
+
+
+# Setup node
+
+rospy.init_node('state_publisher')
+
+rospy.Subscriber('arm/angles', Vector3, _arm_angles_callback)
+rospy.Subscriber('flipper/position', Float32MultiArray, _flippers_callback)
+
+joint_pub = rospy.Publisher('joint_states', JointState, queue_size=1)
+
+# Publish position and joints
+
+broadcaster = tf.TransformBroadcaster()
+
+joint_state = JointState()
+
+rate = rospy.Rate(10)
 while not rospy.is_shutdown():
     # Update joint state
     joint_state.header.stamp = rospy.Time.now()
-    joint_state.name = joint_names
-    joint_state.position = joint_angles
+    joint_state.name = joints.keys()
+    joint_state.position = joints.values()
     joint_pub.publish(joint_state)
 
     broadcaster.sendTransform(
@@ -39,10 +73,4 @@ while not rospy.is_shutdown():
         'base_link',
         'world')
 
-    robot_x += np.cos(robot_angle)*0.05
-    robot_y += np.sin(robot_angle)*0.05
-    robot_angle += np.radians(5)
-    joint_angles[0] = (joint_angles[0]+np.radians(10))%(2*np.pi) 
-    joint_angles[2] = (joint_angles[2]+np.radians(20))%(2*np.pi)
-    
     rate.sleep()
