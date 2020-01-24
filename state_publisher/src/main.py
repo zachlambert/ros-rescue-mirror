dae76cdc040be2c129d6e53badf99e5cfb92e829
Zach Lambert <zachalambert@gmail.com>
2020-01-24 14:29:35 +0000

    Tidied up, added end effector pos publisher.

--- state_publisher/src/main.py
+++ state_publisher/src/main.py
@@ -4,8 +4,17 @@ import rospy
 
 import handlers
 import state_publisher
+import info_publisher
 
 if __name__=="__main__":
     rospy.init_node('state_publisher')
     handlers.init()
-    state_publisher.run()
+    state_publisher.init()
+    info_publisher.init()
+
+    rate = rospy.Rate(20)
+    while not rospy.is_shutdown():
+        state_publisher.publish()
+        info_publisher.publish()
+
+        rate.sleep()
--- state_publisher/src/state_publisher.py
+++ state_publisher/src/state_publisher.py
@@ -6,7 +6,6 @@ from sensor_msgs.msg import JointState
 
 # Callback functions
 
-robot_pose = Pose()
 
 joints = {
     'Rev3': 0,
@@ -29,41 +28,39 @@ def set_arm_angles(angle1, angle2, angle3):
     joints['Rev8'] = angle2
     joints['Rev9'] = angle3
 
-def initialise_robot_pose():
-    global robot_pose
-    robot_pose.position.x=0
-    robot_pose.position.y=0
-    robot_pose.position.z=0
+def get_initial_robot_pose():
+    pose = Pose()
+    pose.position.x=0
+    pose.position.y=0
+    pose.position.z=0
     quaternion = tf.transformations.quaternion_from_euler(0, 0, 0)
-    robot_pose.orientation.x = quaternion[0]
-    robot_pose.orientation.y = quaternion[1]
-    robot_pose.orientation.z = quaternion[2]
-    robot_pose.orientation.w = quaternion[3]
+    pose.orientation.x = quaternion[0]
+    pose.orientation.y = quaternion[1]
+    pose.orientation.z = quaternion[2]
+    pose.orientation.w = quaternion[3]
+    return pose
 
-def run():
-    initialise_robot_pose()
-
-    broadcaster = tf.TransformBroadcaster()
+def init():
+    global joint_state, robot_pose, tf_broadcaster, base_transform, joint_pub
     joint_state = JointState()
 
-    transform = TransformStamped()
-    transform.header.frame_id = 'world'
-    transform.child_frame_id = 'base_link'
+    robot_pose = get_initial_robot_pose()
+    tf_broadcaster = tf.TransformBroadcaster()
+
+    base_transform = TransformStamped()
+    base_transform.header.frame_id = 'world'
+    base_transform.child_frame_id = 'base_link'
 
     joint_pub = rospy.Publisher('joint_states', JointState, queue_size=1)
 
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        # Update joint state
-        joint_state.header.stamp = rospy.Time.now()
-        joint_state.name = joints.keys()
-        joint_state.position = joints.values()
-        joint_pub.publish(joint_state)
+def publish():
+    joint_state.header.stamp = rospy.Time.now()
+    joint_state.name = joints.keys()
+    joint_state.position = joints.values()
+    joint_pub.publish(joint_state)
 
-        transform.header.stamp = rospy.Time.now()
-        transform.transform.translation = robot_pose.position
-        transform.transform.rotation = robot_pose.orientation
+    base_transform.header.stamp = rospy.Time.now()
+    base_transform.transform.translation = robot_pose.position
+    base_transform.transform.rotation = robot_pose.orientation
 
-        broadcaster.sendTransformMessage(transform)
-
-        rate.sleep()
+    tf_broadcaster.sendTransformMessage(base_transform)
