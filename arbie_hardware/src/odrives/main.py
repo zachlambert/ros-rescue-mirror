f2ccc9aaa008c69f7b88327f3e1a82adace0c3ec
Zach Lambert <zachalambert@gmail.com>
2020-10-18 15:42:04 +0100

    Started adding odrive code to arbie_hardware

--- arbie_hardware/src/odrives/main.py
+++ arbie_hardware/src/odrives/main.py
@@ -4,12 +4,21 @@ from rescue_hardware.srv import (
     ReadHardware, ReadHardwareResponse,
     WriteHardware, WriteHardwareResponse
 )
+from odrive_functions import OdriveController
 
 # Setup hardware interface
 
-hardware_connected = False
-print("Flippers hardware not connected")
-print("Tracks hardware not connected")
+# TODO: flippers = OdriveController(rospy.get_param("~flippers_serial_no")
+flippers = OdriveController("336631563536")
+
+# TODO: flippers = OdriveController(rospy.get_param("~flippers_serial_no")
+flippers = OdriveController("209137933548")
+
+if not flippers.is_connected():
+    print("Flippers hardware not connected")
+
+if not flippers.is_connected():
+    print("Flippers hardware not connected")
 
 
 # Setup ROS interface
@@ -33,7 +42,7 @@ tracks_res.vel.data = [0, 0]
 tracks_res.eff.data = [0, 0]
 
 def flippers_read(req):
-    if hardware_connected:
+    if flippers.is_connected():
         pass # TODO
     flippers_res.success = True
     return flippers_res
@@ -68,4 +77,14 @@ def main():
     rospy.Service("flippers/write", WriteHardware, flippers_write)
     rospy.Service("tracks/read", ReadHardware, tracks_read)
     rospy.Service("tracks/write", WriteHardware, tracks_write)
-    rospy.spin()
+
+    flippers_diagnostic_pub = rospy.Publisher(
+        "flippers/diagnostic", DiagnosticStatus, 10)
+    tracks_diagnostic_pub = rospy.Publisher(
+        "flippers/diagnostic", DiagnosticStatus, 10)
+
+    rate = rospy.Rate(1);
+    while not rospy.is_shutdown():
+        flippers_diagnostic_pub.publish(flippers.get_diagnostics_message);
+        tracks_diagnostic_pub.publish(tracks.get_diagnostics_message);
+        rate.sleep()
