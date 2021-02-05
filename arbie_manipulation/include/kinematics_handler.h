1633d16ce6a91d1e093c2058e196f088974eb7aa
Zach Lambert <zachalambert@gmail.com>
2021-02-05 12:44:26 +0000

    Rough outline of code

--- arbie_manipulation/include/kinematics_handler.h
+++ arbie_manipulation/include/kinematics_handler.h
@@ -13,52 +13,6 @@
 #include "tf2_eigen/tf2_eigen.h"
 #include <Eigen/Geometry>
 
-struct ArmState {
-    std::vector<double> gripper_coords;
-    std::vector<double> joint_angles;
-    std::size_t fail_count;
-    ArmState(): gripper_coords(6),joint_angles(6) {}
-};
-struct ArmStateBuffer {
-    static constexpr std::size_t BUFFER_SIZE = 20;
-    std::size_t fail_threshold;
-
-    static constexpr double TRIAL_STEP_MULTIPLIER = 2.0;
-    std::vector<double> trial_gripper_coords;
-
-    ArmState buffer[BUFFER_SIZE];
-    int head;
-    int tail;
-
-    ArmStateBuffer(std::size_t fail_threshold = 10):
-        fail_threshold(fail_threshold), trial_gripper_coords(6), head(0),tail(0)
-    {}
-
-    bool empty() {
-        return head == tail;
-    }
-    ArmState& current() {
-        // Only valid if not empty
-        return buffer[(tail-1) % BUFFER_SIZE];
-    }
-    ArmState& next() {
-        return buffer[tail];
-    }
-    void increment() { 
-        tail = (tail+1) % BUFFER_SIZE;
-        if (head == tail) {
-            head = (head+1) % BUFFER_SIZE;
-        }
-    }
-    void decrement() {
-        if (!empty())
-            tail = (tail-1) % BUFFER_SIZE;
-    }
-    void reset() {
-        head = tail;
-    }
-};
-
 class KinematicsHandler{
 public:
     KinematicsHandler(ros::NodeHandle& n);
@@ -66,7 +20,7 @@ public:
     void joint_state_callback(sensor_msgs::JointState joint_state_msg);
     void velocity_callback(std_msgs::Float64MultiArray velocity_msg);
     void loop(const ros::TimerEvent &timer);
-    
+
     bool gripper_command(const std::string &pose_name);
 
 private:
@@ -82,10 +36,7 @@ private:
         const robot_state::JointModelGroup *joint_group,
         const double *joint_angles);
 
-    bool validate_velocity(double dt);
-    bool validate_state(const Eigen::Isometry3d &gripper_pose);
-    void update_position(double dt);
-    void velocity_failed();
+    bool validate_state();
 
     robot_model_loader::RobotModelLoader robot_model_loader;
     robot_model::RobotModelPtr kinematic_model;
@@ -95,8 +46,6 @@ private:
     collision_detection::CollisionRequest c_req;
     collision_detection::CollisionResult c_res;
     planning_scene::PlanningScene* g_planning_scene;
-
-    ArmStateBuffer arm_state_buffer;
 
     ros::Subscriber joint_state_sub;
     sensor_msgs::JointState joint_state_actual;
--- arbie_manipulation/src/kinematics_handler.cpp
+++ arbie_manipulation/src/kinematics_handler.cpp
@@ -5,7 +5,7 @@
 #include <math.h>
 #include <string>
 
-
+/*
 void quaternion_to_euler(
         Eigen::Quaterniond &q,
         double *roll, double *pitch, double *yaw)
@@ -18,7 +18,6 @@ void quaternion_to_euler(
         2*(q.x()*q.y() + q.z()*q.w()),
         1 - 2*(pow(q.y(), 2) + pow(q.z(), 2)));
 }
-
 
 Eigen::Isometry3d KinematicsHandler::gripper_coords_to_pose(
     const std::vector<double> &gripper_coords)
@@ -67,6 +66,7 @@ void KinematicsHandler::gripper_pose_to_coords(
         &gripper_coords[3], &gripper_coords[4], &gripper_coords[5]
     );
 }
+*/
 
 
 KinematicsHandler::KinematicsHandler(ros::NodeHandle& n):
@@ -117,56 +117,6 @@ KinematicsHandler::KinematicsHandler(ros::NodeHandle& n):
         "arm_position_controller/command",
         1000
     );
-
-    // May need to use the below to do IK near the base position.
-    // Alternatively, may choose to use moveit to return to home.
-    effective_arm_length =
-        kinematic_model->getLinkModel("arm_upper_link")
-        ->getJointOriginTransform().translation().norm();
-}
-
-void KinematicsHandler::initialise_arm_state()
-{
-    // Won't update until it receives the first joint_state message
-    for (std::size_t i = 0; i < joint_state_actual.name.size(); i++) {
-        kinematic_state->setJointPositions(
-            joint_state_actual.name[i], &joint_state_actual.position[i]
-        );
-        ROS_INFO("Joint %s = %f", joint_state_actual.name[i].c_str(),
-                joint_state_actual.position[i]);
-    }
-
-    // TODO: Tidy up the block below
-    static const std::size_t arm_joint_indexes[] = {0, 1, 2, 7, 8, 9};
-    for (std::size_t i = 0; i < 6; i++) {
-        arm_state_buffer.next().joint_angles[i] =
-            joint_state_actual.position[arm_joint_indexes[i]];
-    }
-
-    // Get the relative gripper pose
-    Eigen::Isometry3d gripper_pose_absolute =
-        kinematic_state->getFrameTransform("gripper_base_link");
-
-    // Clear arm state buffer
-    arm_state_buffer.reset();
-
-    // Convert this gripper pose to euler coordinates
-    gripper_pose_to_coords(
-        gripper_pose_absolute,
-        arm_state_buffer.next().gripper_coords
-    );
-    arm_state_buffer.next().fail_count = 0;
-
-    arm_state_buffer.increment();
-
-    ROS_INFO("Pos = %f, %f, %f",
-        arm_state_buffer.current().gripper_coords[0],
-        arm_state_buffer.current().gripper_coords[1],
-        arm_state_buffer.current().gripper_coords[2]);
-    ROS_INFO("Euler = %f, %f, %f",
-        arm_state_buffer.current().gripper_coords[3],
-        arm_state_buffer.current().gripper_coords[4],
-        arm_state_buffer.current().gripper_coords[5]);
 }
 
 void KinematicsHandler::joint_state_callback(
@@ -185,7 +135,7 @@ void KinematicsHandler::joint_state_callback(
     //     "track_right", &joint_state_msg.position[6]);
 
     if (!joints_updated) {
-        initialise_arm_state();
+        // TODO: Copy joint_state_msg positions for arm joints
         joints_updated = true;
     }
 }
@@ -209,137 +159,89 @@ void KinematicsHandler::velocity_callback(
 void KinematicsHandler::loop(const ros::TimerEvent &timer)
 {
     double dt = ros::Duration(timer.current_real - timer.last_real).toSec();
-
     if (!joints_updated || velocity_is_zero) return;
 
-    // Don't update until joints have been updated with actual values
-    if (validate_velocity(dt)) {
-        update_position(dt);
-    } else {
-        ROS_INFO("Velocity failed");
-        velocity_failed();
-    }
-}
+    // Assume dt is small enough that a single step is accurate
+    // ie: theta_next = theta + dt * theta_vel
+    // (instead of interpolating)
 
-bool KinematicsHandler::validate_velocity(double dt)
-{
-    // Compute the gripper coordinates to test, should be further than the
-    // actual next coordinates, to avoid getting too close to a collision state
-    for (std::size_t i = 0; i < 6; i++) {
-        arm_state_buffer.trial_gripper_coords[i] =
-            arm_state_buffer.current().gripper_coords[i]
-            + dt * gripper_velocity[i]
-                 * arm_state_buffer.TRIAL_STEP_MULTIPLIER;
-        ROS_INFO("%lu: %f -> %f", i, arm_state_buffer.current().gripper_coords[i], arm_state_buffer.trial_gripper_coords[i]);
-    }
+    // TODO: Make this a private variable, which gets updated each loop and
+    // initialised by joint_state_callback when necessary
+    std::vector<double> joint_pos(6);
+    // TODO: Compare joint_pos with actual joint states. If these lag behind
+    // the target joint states too much, adjust targets
 
-    Eigen::Isometry3d gripper_pose = gripper_coords_to_pose(
-        arm_state_buffer.trial_gripper_coords);
+    kinematic_state->setJointGroupPositions(joint_model_group, joint_pos);
 
-    return validate_state(gripper_pose);
-}
+    Eigen::VectorXd ee_vel, joint_vel;
+    // TODO: Make this a private variable which gets updated with gripper velocity callback
+    // TODO: Transform from polar coordinates to cartesian coordinates
+    ee_vel << 0, 0, 0, 0, 0, 0;
 
-void KinematicsHandler::update_position(double dt)
-{
-    // The above state was calculated using the trial coordinates
-    // Now, need to calculate and verify the actual next state
-    // If the trial state is valid, the next state is almost always
-    // going to be valid, but should still check in the rare case that
-    // it isn't
-
-    std::vector<double> &current_gripper_coords =
-        arm_state_buffer.current().gripper_coords;
-    std::vector<double> &next_gripper_coords =
-        arm_state_buffer.next().gripper_coords;
-    for (std::size_t i = 0; i < 6; i++) {
-        next_gripper_coords[i] = (current_gripper_coords[i]
-            + dt * gripper_velocity[i]);
-        // Normalise to [-pi, pi]
-        while (next_gripper_coords[i] < -M_PI) next_gripper_coords[i] += 2*M_PI;
-        while (next_gripper_coords[i] > M_PI) next_gripper_coords[i] -= 2*M_PI;
-    }
-
-    Eigen::Isometry3d gripper_pose = gripper_coords_to_pose(next_gripper_coords);
-
-    // If the velocity is valid, it is very likely that the next valid too,
-    // but should still check
-    // if (!validate_state(gripper_pose)) {
-    //     return;
-    // }
-
-    kinematic_state->copyJointGroupPositions(
-        joint_model_group, arm_msg.data);
-    arm_pub.publish(arm_msg);
-
-    copy(
-        arm_msg.data.cbegin(), arm_msg.data.cend(),
-        arm_state_buffer.next().joint_angles.begin()
+    Eigen::MatrixXd jacobian;
+    Eigen::Vector3d reference_point_position(0, 0, 0);
+    kinematic_state->getJacobian(
+        joint_model_group,
+        kinematic_state->getLinkModel(joint_model_group->getLinkModelNames().back()),
+        reference_point_position,
+        jacobian
     );
+    joint_vel = jacobian.inverse() * ee_vel;
 
-    arm_state_buffer.current().fail_count = 0;
-    arm_state_buffer.increment();
-}
-
-void KinematicsHandler::velocity_failed()
-{
-    arm_state_buffer.current().fail_count++;
-
-    if (arm_state_buffer.current().fail_count > arm_state_buffer.fail_threshold) {
-        // Although the current state is valid, for some reason, it is stuck and
-        // no nearby states are valid. Therefore, move back to a known valid state.
-        ROS_INFO("Current state exceeded fail count");
-
-        arm_state_buffer.decrement();
-
-        // If there are no valid states left then it is stuck.
-        // Shouldn't get here, but need to make sure.
-        if (arm_state_buffer.empty()) {
-            arm_state_buffer.current().fail_count = 0;
-            joints_updated = false;
+    // Check that velocity limits aren't exceeded
+    kinematic_state->setJointGroupVelocities(joint_model_group, joint_vel.data());
+    for (auto &joint_model: joint_model_group->getJointModels()) {
+        if (!kinematic_state->satisfiesVelocityBounds(joint_model)) {
+            ROS_INFO("Doesn't satisfy velocity bounds.");
             return;
         }
+    }
 
-        // Load the previous state
-        copy(
-            arm_state_buffer.current().joint_angles.cbegin(),
-            arm_state_buffer.current().joint_angles.cend(),
-            arm_msg.data.begin()
+    // trial_joint_pos = joint_pos + dt * joint_vel
+    // joint_pos will be updated to these values if the current velocity
+    // doesn't cause a collision
+    std::vector<double> trial_joint_pos(joint_pos.size());
+    for (std::size_t i = 0; i < trial_joint_pos.size(); i++) {
+        trial_joint_pos[i] = joint_pos[i] + joint_vel[i] * dt;
+    }
+
+    // To check if the current velocity causes a collision, move with this
+    // velocity for a given period of time T, then check if the final state
+    // is valid.
+
+    std::vector<double> query_joint_pos = trial_joint_pos;
+
+    double t = dt;
+    const double T = 0.25;
+    while (t < T) {
+        kinematic_state->setJointGroupPositions(joint_model_group, query_joint_pos);
+        kinematic_state->getJacobian(
+            joint_model_group,
+            kinematic_state->getLinkModel(joint_model_group->getLinkModelNames().back()),
+            reference_point_position,
+            jacobian
         );
-        arm_pub.publish(arm_msg);
+        joint_vel = jacobian.inverse() * ee_vel;
+        for (std::size_t i = 0; i < query_joint_pos.size(); i++) {
+            query_joint_pos[i] += joint_vel[i] * dt;
+        }
+        t += dt;
     }
+    kinematic_state->setJointGroupPositions(joint_model_group, query_joint_pos);
+    if (!validate_state()) {
+        ROS_INFO("Velocity failed");
+        return;
+    }
+
+    joint_pos = trial_joint_pos;
+    // TODO: Publish joint commands
 }
 
-bool KinematicsHandler::ik_constraint(
-    robot_state::RobotState *robot_state,
-    const robot_state::JointModelGroup *joint_group,
-    const double *joint_angles)
+bool KinematicsHandler::validate_state()
 {
-    static const double MAX_ANGLE_CHANGE = M_PI/4;
-    double angle_dif;
-    // for (std::size_t i = 0; i < 6; i++) {
-    //     angle_dif = fabs(joint_angles[i] - arm_state_buffer.current().joint_angles[i]);
-    //     if (angle_dif > MAX_ANGLE_CHANGE && angle_dif < 2*M_PI - MAX_ANGLE_CHANGE) {
-    //         ROS_INFO("Joint %lu: %f vs %f", i, joint_angles[i], arm_state_buffer.current().joint_angles[i]);
-    //         return false;
-    //     }
-    // }
-    return true;
-}
+    // validates the current state of kinematic_state, so assumes
+    // kinematic_state has already been set with positions and velocities
 
-bool KinematicsHandler::validate_state(const Eigen::Isometry3d &gripper_pose)
-{
-    static const float IK_TIMEOUT = 0.2;
-
-    static const moveit::core::GroupStateValidityCallbackFn constraint =
-        boost::bind(&KinematicsHandler::ik_constraint, this, _1, _2, _3);
-
-    bool found_ik = kinematic_state->setFromIK(
-        joint_model_group, gripper_pose, IK_TIMEOUT, constraint
-    );
-    if (!found_ik) {
-        ROS_INFO("IK solution not found");
-        return false;
-    }
     if(!kinematic_state->satisfiesBounds()){
         ROS_INFO("IK solution found but exceeds joint limits");
         return false;
@@ -371,7 +273,6 @@ bool KinematicsHandler::gripper_command(const std::string &pose_name)
         move_group.move();
         // Reset state history, and invalidate current state, so wait
         // until the state gets updated with actual angles
-        arm_state_buffer.reset();
         joints_updated = false;
         return true;
     }
